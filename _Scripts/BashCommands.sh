ced30497d689a5515c1ded3923cf37821d7d7277
Lawlor-Jim <124158242+Lawlor-Jim@users.noreply.github.com>
2023-05-03 17:33:02 +0000

    Extra changes before assignment submission.

--- _Scripts/BashCommands.sh
+++ _Scripts/BashCommands.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
+# The bash environment variable $PS3 is the prompt used by select. Setting to a new value will cause a new prompt.
 PS3="Select your option please: " 
 
 echo "   * Bash Commads *  "
 
+
+#We display a list of options preceded by numbers.
+#We select "opt" in "set of options", and if the user entered number matches one of the positions, that command is executed.
 select lng in List_Files Show_Free_Disk_Space Show_System_Path Display_Command_History Backup_Files Quit
 do
 
     case $lng in
         "List_Files")
-            echo $(ls);;
+        #echo the ls command, to list files.
+          echo $(ls);;
 
         "Show_Free_Disk_Space")
-           echo $(df);;
+        #echo the df command, to list free disk space.
+          echo $(df);;
 
         "Show_System_Path")
-           echo $(pwd);;
+        #echo the pwd command, to show current path.
+          echo $(pwd);;
 
          "Display_Command_History")
+         #echo the result of using the cat command on the file storing command history.
            echo $(cat ~/.bash_history);;
            
          "Backup_Files")
+         #enter the file name to be backed up, and back up recursively to BackupFolder.
            echo "Please enter directory to back up:"
            read FolderName
            $cat >BackupFolder
-           cp -R $FolderName backup;;
+           cp -R $FolderName BackupFolder;;
 
          "Quit")
+         #Exit program.
            echo "Goodbye!"
            break;;
         *)
            echo "Invalid"
+          #If the selected option is invalid, we display the menu options again.
            echo "1) List_Files
 2) Show_Free_Disk_Space
 3) Show_System_Path
--- ex5.sh
+++ ex5.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
-# The bash environment variable $PS3 is the prompt 
-# used by select . Set it to a new value and you'll get a new prompt.
-PS3="Select your language please: "
+# The bash environment variable $PS3 is the prompt used by select. Set it to a new value and you'll get a new prompt.
+PS3="Select your option please: "
 
-# We should use the select command to create a simple menu 
-# in the terminal. Then, the command displays a list of options preceded by numbers.
 
-# select NAME [in WORDS ... ;] do COMMANDS; done
-# select repeatedly reads a number from standard input. Subsequently, if the number corresponds 
-# to a string’s position in WORDS, the command sets NAME to the respective text. Otherwise, nothing is set.
-select lng in DiskUsage Users Uptime Quit
+#We display a list of options preceded by numbers.
+#We select "opt" in "set of options", and if the user entered number matches one of the positions, that command is executed.
+select opt in DiskUsage Users Uptime Quit
 do
-    case $lng in
+    case $opt in
         "DiskUsage")
-            echo $"df";;
+            echo "$(df)";;
         "Users")
-           echo "$uptime";;
+           echo "$(who -H)";;
         "Uptime")
-           echo "$who";;
+           echo "$(uptime)";;
         "Quit")
            echo "Exiting..."
            break;;
